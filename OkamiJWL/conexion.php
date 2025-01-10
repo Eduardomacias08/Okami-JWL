6631f03d9463b47a0b8321ab399309f8b2d089c4
Eduardo Macias "MACDEV <93840060+Eduardomacias08@users.noreply.github.com>
2025-01-10 16:25:43 -0600

    Update conexion.php

--- OkamiJWL/conexion.php
+++ OkamiJWL/conexion.php
@@ -1,9 +1,14 @@
-$servername = "autorack.proxy.rlwy.net"; 
-$username = "root";        
-$password = "YpeFZHtOVQXTBdGXiJqbJsEvxpFATZAa";        
-$dbname = "railway"; 
+<?php
+$servername = "autorack.proxy.rlwy.net"; // El host proporcionado por Railway
+$username = "root";                     // El usuario de MySQL (en tu caso es "root")
+$password = "YpeFZHtOVQXTBdGXiJqbJsEvxpFATZAa"; // La contraseña proporcionada
+$dbname = "railway";                    // El nombre de la base de datos en Railway
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname, 12423); // Asegúrate de incluir el puerto 12423
+
+// Comprobar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+?>
