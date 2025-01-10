139cfa55c6aec8bbc66d5715dc8407247ba70988
Eduardo Macias "MACDEV <93840060+Eduardomacias08@users.noreply.github.com>
2025-01-10 15:05:32 -0600

    Update conexion.php
    haciendo pruebas de configuracion a base de datos

--- OkamiJWL/conexion.php
+++ OkamiJWL/conexion.php
@@ -1,14 +1,17 @@
 <?php
-$servername = "autorack.proxy.rlwy.net"; // O la dirección de tu servidor
-$username = "root";        // Tu usuario de MySQL
-$password = "YpeFZHtOVQXTBdGXiJqbJsEvxpFATZAa";        // Tu contraseña de MySQL
-$dbname = "railway"; // El nombre de tu base de datos
+$servername = "autorack.proxy.rlwy.net";
+$username = "root";
+$password = "YpeFZHtOVQXTBdGXiJqbJsEvxpFATZAa";
+$dbname = "railway";
+$port = 12423; // Puerto de conexión
 
 // Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Comprobar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
+} else {
+    echo "Conexión exitosa a la base de datos.";
 }
 ?>
