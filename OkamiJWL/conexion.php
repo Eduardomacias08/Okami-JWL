51232ddd645f448bd17b5ce661fc3ac870bd9167
Eduardo Macias "MACDEV <93840060+Eduardomacias08@users.noreply.github.com>
2025-01-10 15:30:40 -0600

    Update conexion.php
    configuracion de la base de datos

--- OkamiJWL/conexion.php
+++ OkamiJWL/conexion.php
@@ -1,17 +1,9 @@
-<?php
-$servername = "autorack.proxy.rlwy.net";
-$username = "root";
-$password = "YpeFZHtOVQXTBdGXiJqbJsEvxpFATZAa";
-$dbname = "railway";
-$port = 3306; // Puerto de conexión
+$servername = "autorack.proxy.rlwy.net"; 
+$username = "root";        
+$password = "YpeFZHtOVQXTBdGXiJqbJsEvxpFATZAa";        
+$dbname = "railway"; 
 
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
-
-// Comprobar la conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
-} else {
-    echo "Conexión exitosa a la base de datos.";
 }
-?>
