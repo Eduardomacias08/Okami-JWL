1ee7e940a68286e3b04ff48f496db07d51490f43
Eduardo Macias "MACDEV <bryanwesly8@gmail.com>
2025-01-13 14:47:42 -0600

    pruebas

--- OkamiJWL/conexion.php
+++ OkamiJWL/conexion.php
@@ -9,7 +9,7 @@ try {
     // Estableciendo la conexión
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexion exitosa";
+    // echo "Conexion exitosa";
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
