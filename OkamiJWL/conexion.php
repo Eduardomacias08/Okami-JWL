b35457e68beba2c7f127f412a2ffff25cd324fde
Eduardo Macias "MACDEV <bryanwesly8@gmail.com>
2025-01-13 13:05:58 -0600

    prueba

--- OkamiJWL/conexion.php
+++ OkamiJWL/conexion.php
@@ -1,14 +1,14 @@
 <?php
-$host = getenv('autorack.proxy.rlwy.net');
-$port = getenv('12423');
-$dbname = getenv('railway');
-$username = getenv('root');
-$password = getenv('YpeFZHtOVQXTBdGXiJqbJsEvxpFATZAa');
+$host = 'autorack.proxy.rlwy.net';
+$port = 12423;
+$dbname = 'railway';
+$user = 'root';
+$password = 'YpeFZHtOVQXTBdGXiJqbJsEvxpFATZAa';
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Conexión exitosa a la base de datos.";
 } catch (PDOException $e) {
-    echo "Error al conectar a la base de datos: " . $e->getMessage();
+    echo "Error de conexión: " . $e->getMessage();
 }
