0429432fef0e73b75a51e9817205ba591c1bc2f7
Eduardo Macias "MACDEV <bryanwesly8@gmail.com>
2025-01-13 13:40:23 -0600

    prueba

--- OkamiJWL/index.php
+++ OkamiJWL/index.php
@@ -7,7 +7,7 @@ require 'conexion.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catálogo de Venta de Plata</title>
+    <title>Catálogo de Venta de Plata prueba</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
