4c86c3827f5845b5a6b67acb06f2f21cb65ee753
Eduardomacias08 <bryanwesly8@gmail.com>
2025-01-23 13:48:28 -0600

    conexion a base de datos local

--- OkamiJWL/conexion.php
+++ OkamiJWL/conexion.php
@@ -1,15 +1,15 @@
 <?php
-$host = 'autorack.proxy.rlwy.net';  // Host proporcionado por Railway
-$port = '12423';                    // Puerto proporcionado por Railway
-$dbname = 'venta_plata';            // Cambia el nombre de la base de datos a "venta_plata"
-$user = 'root';                     // Usuario de la base de datos
-$password = 'YpeFZHtOVQXTBdGXiJqbJsEvxpFATZAa';  // Contraseña proporcionada
+$host = 'localhost';
+$port = '3306'; // Si no cambiaste el puerto.
+$dbname = 'venta_plata_local'; // Cambia al nombre de tu base de datos.
+$user = 'root';
+$password = 'root'; // Deja vacío si no configuraste una contraseña en MySQL.
 
 try {
-    // Estableciendo la conexión
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Conexion exitosa";
+    // echo "Conexión exitosa";
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
+?>
